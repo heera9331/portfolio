62425a7c529b76a54feca8acc58be9fdccd5e518
heera9331 <heera9331@gmail.com>
2024-04-20 21:33:13 +0530

    new portfolio initialized

---
+++ next-portfolio/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Heera's Portfolio",
+  description: "Heera is software developer that develop effficent software systems.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ next-portfolio/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl font-semibold p-10 text-orange-600">Heera Singh Lodhi</h1>
+      <h1 className="text-3xl font-semibold p-10 text-yellow-600">Heera Singh Lodhi</h1>
+    </main>
+  );
+}
