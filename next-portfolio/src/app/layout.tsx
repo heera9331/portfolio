fedfc042687753c5df8b20b7c9c5e96a52e4b2cf
heera9331 <heera9331@gmail.com>
2024-04-20 22:27:49 +0530

    basic setup

--- next-portfolio/src/app/layout.tsx
+++ next-portfolio/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[var(--primary)] text-[var(--main)]`}>{children}</body>
     </html>
   );
 }
--- next-portfolio/src/app/page.tsx
+++ next-portfolio/src/app/page.tsx
@@ -2,9 +2,45 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-3xl font-semibold p-10 text-orange-600">Heera Singh Lodhi</h1>
-      <h1 className="text-3xl font-semibold p-10 text-yellow-600">Heera Singh Lodhi</h1>
+    <main className="font-sans mx-10">
+      <div className="grid grid-cols-2 my-10 h-[80vh] border-2">
+        <div className="flex justify-center flex-col">
+          <h1 className="text-3xl font-semibold uppercase text-[var(--main)]">
+            Hello, my name is Heera Singh
+          </h1>
+          <h2 className="text-2xl font-semibold uppercase">
+            I'm a Software Developer
+          </h2>
+          <div className="text-[18px] flex flex-col gap-4 justify-around my-4">
+            <p className="">
+              I am Heera Singh Lodhi, a passionate and dedicated fresher with a
+              strong foundation in computer science and engineering.{" "}
+            </p>
+            <p>
+              I have a keen interest in programming and possess solid knowledge
+              of object-oriented programming, data structures and algorithms,
+              HTML, CSS, JavaScript, WordPress, C programming, Java, and Python.
+            </p>
+            <p>
+              With a creative mindset and strong problem-solving skills, I
+              strive to contribute my skills and enthusiasm to challenging
+              projects.
+            </p>
+          </div>
+          <button className="bg-[var(--ternary)] text-white font-semibold uppercase py-2 px-3 rounded-md hover:bg-blue-600 inline w-40">
+            More About Me
+          </button>
+        </div>
+        <div className="border-2 h-full p-10 flex items-center justify-center">
+          <Image
+            src={`/media/images/heera-singh-bg.png`}
+            width={"800"}
+            height={"800"}
+            alt="Heera Singh"
+            className="w-[60%]"
+          />
+        </div>
+      </div>
     </main>
   );
 }
